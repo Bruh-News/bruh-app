f0eca3f1c072e8fb59e8a2ba9bcea2cf79d523f5
Reetik Patel <Reetik2000@yahoo.com>
2021-11-04 21:54:50 -0500

    🐛 Fixed card story

--- app/components/card/card.story.tsx
+++ app/components/card/card.story.tsx
@@ -4,6 +4,8 @@ import { storiesOf } from "@storybook/react-native"
 import { StoryScreen, Story, UseCase } from "../../../storybook/views"
 import { Card } from "./card"
 
+declare let module
+
 storiesOf("Card", module)
   .addDecorator((fn) => <StoryScreen>{fn()}</StoryScreen>)
   .add("Style Presets", () => (
