0d4c5f6daa06323561acd48c059cbe1c8199d176
Reetik Patel <Reetik2000@yahoo.com>
2021-12-04 23:27:03 -0600

    🚧 Model for user storage functions

---
+++ app/models/user-store/user-store.ts
@@ -0,0 +1,38 @@
+import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { withEnvironment } from "../extensions/with-environment";
+import { UserModel, UserSnapshot } from "../user/user";
+import { UserAPI } from "../../services/api/user-api"
+
+/**
+ * Model description here for TypeScript hints.
+ */
+export const UserStoreModel = types
+  .model("UserStore")
+  .props({
+    user: types.maybeNull(UserModel)
+  })
+  .extend(withEnvironment)
+  .actions((self) => ({
+    saveUser: (user: UserSnapshot) => {
+      self.user = user;
+    }
+  }))
+  .actions((self) => ({
+    getUser: async (userId: number) => {
+      const userAPI = new UserAPI(self.environment.api);
+      const result = await userAPI.getUser(userId);
+
+      if(result.kind === "ok") {
+        const user: UserSnapshot = result.user;
+        self.saveUser(user);
+      } else {
+        __DEV__ && console.tron.log(result.kind);
+      }
+    }
+  }));
+
+type UserStoreType = Instance<typeof UserStoreModel>
+export interface UserStore extends UserStoreType {}
+type UserStoreSnapshotType = SnapshotOut<typeof UserStoreModel>
+export interface UserStoreSnapshot extends UserStoreSnapshotType {}
+export const createUserDefaultModel = () => types.optional(UserStoreModel, {})
