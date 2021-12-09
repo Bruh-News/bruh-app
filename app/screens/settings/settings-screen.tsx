f4be29e8af9bb8532ba4aae5feb41a813a2ffedb
Reetik Patel <Reetik2000@yahoo.com>
2021-12-09 17:27:10 -0600

    🚧 Functioning settings

--- app/screens/settings/settings-screen.tsx
+++ app/screens/settings/settings-screen.tsx
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ViewStyle } from "react-native"
-import { Screen, Text } from "../../components"
+import { ActivityIndicator, ViewStyle } from "react-native"
+import { Button, Screen, Text, TextField } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
 import { color } from "../../theme"
+import * as Storage from "../../utils/storage";
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.offWhite,
@@ -17,9 +18,63 @@ export const SettingsScreen = observer(function SettingsScreen() {
 
   // Pull in navigation via hook
   // const navigation = useNavigation()
+  const [numPosts, setNumPosts] = useState(7);
+  const [loading, setLoading] = useState(false);
+  const [touched, setTouched] = useState(false);
+
+  const save = async () => {
+    await Storage.saveString("PostsPerPage", numPosts.toString());
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    Storage.loadString("PostsPerPage")
+      .then((val) => {
+        if(val !== null) {
+          setNumPosts(Number.parseInt(val));
+        } else {
+          throw NaN;
+        }
+        setLoading(false);
+      }).catch(() => {
+        setNumPosts(7);
+        Storage.saveString("PostsPerPage", "7");
+        setLoading(false);
+      });
+  });
+
   return (
     <Screen style={ROOT} preset="scroll">
-      <Text preset="header" text="INSERT SETTINGS HERE" />
+      <Text preset="header" text="Settings" />
+      {
+        loading ?
+          <ActivityIndicator color={color.palette.white} size="large" />
+        :
+          <>
+            <TextField
+              // style={fieldStyle}
+              label="Posts per Page"
+              onChangeText={(val) => {
+                setTouched(true);
+                setNumPosts(Number.parseInt(val))
+              }}
+              value={numPosts.toString()}
+              keyboardType="numeric"
+              editable={!loading}
+            />
+            <Button
+              // style={BUTTON}
+              disabled={!touched}
+              children={
+                loading ?
+                  <ActivityIndicator color={color.palette.white} size="large" />
+                : null
+              }
+              text="Save Changes"
+              onPress={save}
+            />
+          </>
+      }
     </Screen>
   )
 })
