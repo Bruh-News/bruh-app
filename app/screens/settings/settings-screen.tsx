96abbc88d3b7c19809fef71ccc45a58b5c76acd3
Reetik Patel <Reetik2000@yahoo.com>
2021-12-09 18:21:09 -0600

    💄 Settings styles

--- app/screens/settings/settings-screen.tsx
+++ app/screens/settings/settings-screen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { ActivityIndicator, ViewStyle } from "react-native"
+import { ActivityIndicator, TextStyle, View, ViewStyle } from "react-native"
 import { Button, Screen, Text, TextField } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
 // import { useStores } from "../../models"
@@ -10,6 +10,13 @@ import * as Storage from "../../utils/storage";
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.offWhite,
   flex: 1,
+  padding: 16
+}
+const TITLE: TextStyle = {
+  color: color.palette.forest
+}
+const SUBMIT: ViewStyle = {
+  marginTop: 8
 }
 
 export const SettingsScreen = observer(function SettingsScreen() {
@@ -63,7 +70,10 @@ export const SettingsScreen = observer(function SettingsScreen() {
               editable={!loading}
             />
             <Button
-              // style={BUTTON}
+              style={{
+                ...SUBMIT,
+                backgroundColor: touched ? color.palette.lightForest : color.palette.lightGrey
+              }}
               disabled={!touched}
               children={
                 loading ?
